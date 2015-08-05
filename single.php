76e44749ed1a153641b32211e3b7a493153c3ce8
Rupinderwadali <rupinderwadali@gmail.com>
2015-08-05 12:20:01 +0530

    remove sidebar and comments

--- single.php
+++ single.php
@@ -18,9 +18,9 @@ get_header(); ?>
 
 			<?php
 				// If comments are open or we have at least one comment, load up the comment template
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
+			//	if ( comments_open() || get_comments_number() ) :
+			//		comments_template();
+			//	endif;
 			?>
 
 		<?php endwhile; // end of the loop. ?>
@@ -28,5 +28,5 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
+<?php //get_sidebar(); ?>
 <?php get_footer(); ?>
